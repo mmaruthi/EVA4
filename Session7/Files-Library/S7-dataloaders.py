54146bd0ffee9c379abb153c0067145e546729eb
Maruthi Srinivas Mudaragadda <48976860+mmaruthi@users.noreply.github.com>
2020-03-05 15:21:32 +0530

    Update S7-dataloaders.py

--- Session7/Files-Library/S7-dataloaders.py
+++ Session7/Files-Library/S7-dataloaders.py
@@ -1,3 +1,4 @@
+# Import the torch library
 import torch 
 
 # Class to create a Data Loader 
@@ -5,5 +6,20 @@ import torch
 Class DataLoader:
       
     def __init__(self, shuffle, batch_size, seed):
+        self.shuffle = True,
+        self.batch_size = 128,
+        self.seed    = 1
+         
+        cuda = torch.cuda.is_available()
+            
+        if cuda:
+           torch.cuda.manual_seed(seed) # Seed is for reproducibility
+      
+        # # dataloader arguments # # which we load
+        self.dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=4, pin_memory=True) if cuda else dict(shuffle=shuffle, batch_size=batch_size)
+
+        def load(self, data):
+            return torch.utils.data.DataLoader(data, **self.dataloader_args)
+     
         
       
